a4d174ac2d9d863a39393d8b64a4aef209b8b80b
YOGESH <yogrival911@gmail.com>
2021-01-03 23:06:54 +0530

    MyMall

--- app/src/main/java/com/yogdroidtech/mymall/category/CategoryFragment.java
+++ app/src/main/java/com/yogdroidtech/mymall/category/CategoryFragment.java
@@ -3,16 +3,20 @@ package com.yogdroidtech.mymall.category;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.yogdroidtech.mymall.R;
+import com.yogdroidtech.mymall.util.CategoryAdapter;
 
 
 public class CategoryFragment extends Fragment {
-
+RecyclerView recyclerView;
+CategoryAdapter adapter;
     public CategoryFragment() {
         // Required empty public constructor
     }
@@ -21,6 +25,11 @@ public class CategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_category, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
+        adapter = new CategoryAdapter();
+        recyclerView.setAdapter(adapter);
+
         return view;
     }
 }
---
+++ app/src/main/java/com/yogdroidtech/mymall/util/CategoryAdapter.java
@@ -0,0 +1,37 @@
+package com.yogdroidtech.mymall.util;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yogdroidtech.mymall.R;
+
+public class CategoryAdapter  extends RecyclerView.Adapter<CategoryAdapter.MyViewHolder> {
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item_layout, parent,false);
+
+        return new CategoryAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 7;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
