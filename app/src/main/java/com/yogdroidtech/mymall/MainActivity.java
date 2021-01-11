c47a27b80012b0e42da4595ef3320a0f4b015c5a
YOGESH <yogrival911@gmail.com>
2021-01-11 12:58:51 +0530

    MyMall

--- app/src/main/java/com/yogdroidtech/mymall/MainActivity.java
+++ app/src/main/java/com/yogdroidtech/mymall/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +18,7 @@ import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.yogdroidtech.mymall.addresses.AddressFragment;
 import com.yogdroidtech.mymall.category.CategoryFragment;
 import com.yogdroidtech.mymall.favourite.FavFragment;
 import com.yogdroidtech.mymall.home.HomeFragment;
@@ -55,6 +58,37 @@ private ActionBarDrawerToggle toggle;
                 .replace(R.id.frame, new HomeFragment())
                 .commit();
 
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.address:
+                        drawerLayout.closeDrawers();
+                        Fragment fragment = new AddressFragment();
+                        FragmentManager fragmentManager = getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                        fragmentTransaction.replace(R.id.frame, fragment);
+                        fragmentTransaction.commit();
+                        break;
+
+                    case R.id.share:
+                        drawerLayout.closeDrawers();
+                        Intent sharingIntent = new Intent();
+                        sharingIntent.setAction(Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        String shareBody = "Check out VeggieGram";
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "VeggieGram");
+                        sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+                        break;
+
+                }
+
+                return true;
+            }
+        });
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -88,6 +122,7 @@ private ActionBarDrawerToggle toggle;
         getMenuInflater().inflate(R.menu.top_menu,menu);
         return  true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
